8ed71a94021cccaeeeaeee1577f2cd293b2f51d5
Falcunargo9 <160991896+Falcunargo9@users.noreply.github.com>
2024-02-27 18:22:27 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
-let proxyIP = '193.122.93.138'
+let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://1.1.1.1/dns-query';
 // (dohURL) list :
