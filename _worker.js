ea513048b15d18fa50b3184d3e9946b9960dbfd0
Falcunargo9 <160991896+Falcunargo9@users.noreply.github.com>
2024-03-07 09:20:44 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,11 +8,11 @@ let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org','cdn.xn--b6gac.eu.org','cdn-b100.xn--b6gac.eu.org','edgetunnel.anycast.eu.org','cdn.anycast.eu.org','proxyip.aliyun.fxxk.dedyn.io','proxyip.oracle.fxxk.dedyn.io','proxyip.fxxk.dedyn.io', ,'proxyip.sg.fxxk.dedyn.io' , 'proxyip.jp.fxxk.dedyn.io','proxyip.hk.fxxk.dedyn.io','workers.bestip.one'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
-let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
+let proxyIP = '129.151.208.148'
 // use single proxyIP instead of random
-// let proxyIP = 'cdn.xn--b6gac.eu.org';
+// let proxyIP = '129.151.208.148';
 // ipv6 proxyIP example remove comment to use
-// let proxyIP = "[2a01:4f8:c2c:123f:64:5:6810:c55a]"
+// let proxyIP = "[129.151.208.148]"
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
